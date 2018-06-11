dccc9336ca928375bf9db7c24aad8068360cc503
Hari Govind <hgs@disroot.org>
2018-06-11 15:36:49 +0530

    Added rerun gem to development environment

--- Gemfile
+++ Gemfile
@@ -2,3 +2,7 @@ source 'https://rubygems.org'
 ruby '2.5'
 
 gem 'sinatra', '>= 1.4' 
+
+group :development do 
+    gem 'rerun'
+end
--- app.rb
+++ app.rb
@@ -2,6 +2,6 @@ require 'sinatra'
 
 class MyApp < Sinatra::Base
     get '/' do
-        '<h1>Hello World</h1>'
+        '<h1>Learning Sinatra.</h1>'
     end
 end
